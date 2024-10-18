d7d35e107a7cba33be5e61d33c37dd54a5c863b3
mhairscambay <mhos.cambay.up@phinmaed.com>
2024-10-18 13:48:24 +0800

    Add files via upload

---
+++ ScoreRequest.java
@@ -0,0 +1,12 @@
+package com.example.titans_jump.model;
+
+public class ScoreRequest {
+    private String username;
+
+    // Constructor expecting a String only
+    public ScoreRequest(String username) {
+        this.username = username;
+    }
+
+    // Getter and setter can be added here if needed
+}
