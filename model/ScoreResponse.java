1df6b6541c4c0e4769c3af640b271d769f85ef11
mhairscambay <mhos.cambay.up@phinmaed.com>
2024-10-18 13:50:08 +0800

    Add files via upload

---
+++ model/ScoreResponse.java
@@ -0,0 +1,15 @@
+package com.example.titans_jump.model;
+
+
+public class ScoreResponse {
+    private String message; // Assuming your API returns a message
+
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter method (if needed)
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
